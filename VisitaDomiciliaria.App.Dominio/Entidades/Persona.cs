230e456e19c43b217938d543998935895f4b4d4a
NanisRo <90880110+NanisRo@users.noreply.github.com>
2021-10-09 23:36:34 -0500

    Video19 / Function Update RepPropietario
    Creación variable idPropietario en entidad Persona
    Configuración RepositorioPropietario / update

--- VisitaDomiciliaria.App.Dominio/Entidades/Persona.cs
+++ VisitaDomiciliaria.App.Dominio/Entidades/Persona.cs
@@ -4,6 +4,7 @@ namespace VisitaDomiciliaria.App.Dominio
     public class Persona
     {
         public int Id {get;set;}
+        public int idPropietario {get;set;}
         public string Nombres {get;set;}
         public string Apellidos {get;set;}
         public string Telefono {get;set;} 
